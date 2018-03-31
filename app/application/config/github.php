2604dc5e2c076e0cd26a5601c7bb597e58a7662c
zhangzheng1019 <447590461@qq.com>
2018-04-01 01:00:40 +0800

    git

--- app/application/config/github.php
+++ app/application/config/github.php
@@ -2,4 +2,4 @@
 
 //以下设置GitHub参数
 $config['appid']  = '4f456cf7d37139b6dd43';
-$config['appSecret'] = '23ff3ab490b60226440237a1157dd50ae41a8ba5';
+$config['appSecret'] = '26114328d81e1d862608d44e61ef4989bf196815';
